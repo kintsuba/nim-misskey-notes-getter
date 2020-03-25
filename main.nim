fd2a306ea87a2650fb7de4da72a68726318f1f1f
kintsuba <bnxth107@gmail.com>
2020-03-25 14:43:59 +0900

    make loop

--- main.nim
+++ main.nim
@@ -1,5 +1,5 @@
 import misskey
-import json
+import os, json
 import yaml/serialization, streams
 
 type Settings = object
@@ -9,9 +9,32 @@ proc main() =
   var settings: Settings;
   var s = newFileStream("settings.yaml")
   load(s, settings)
-  let notes = getNotes(settings.token, "5acddf5fcd7aebd97984769a", 5).getElems();
-  
-  for i, note in notes:
-    echo note
+  const meimeiToken = "5acddf5fcd7aebd97984769a"
 
+  var
+    getNotesCount = 300
+    lastNotesId: string
+  
+  while getNotesCount > 0:
+    let notes = if lastNotesId == "":
+      getNotes(settings.token, meimeiToken, 100).getElems()
+    else:
+      getNotes(settings.token, meimeiToken, 100, lastNotesId).getElems()
+
+    var text: string
+    for note in notes:
+      text = text & note["text"].getStr & "\n"
+      
+    block:
+      let f: File = if lastNotesId == "":
+        open("notes.txt", FileMode.fmWrite)
+      else:
+        open("notes.txt", FileMode.fmAppend)
+      defer:
+        close(f)
+      f.writeLine(text)
+
+    lastNotesId = notes[99]["id"].getStr
+    getNotesCount -= 100
+    sleep(10000)
 main()
--- misskey.nim
+++ misskey.nim
@@ -4,8 +4,28 @@ import httpClient, json
 let client = newHttpClient()
 client.headers = newHttpHeaders({ "Content-Type": "application/json" })
 
-proc getNotes*(token: string, userId: string, limit: int, sinceId: string, untilId: string) =
-  echo token & userId & limit.intToStr() & sinceId & untilId
+proc requestMisskey*(url: string, httpMethod: HttpMethod, body: string): JsonNode =
+  let response = client.request("https://misskey.m544.net/api/users/notes", httpMethod = HttpPost, body = $body)
+  return response.body.parseJson()
+
+proc getNotes*(token: string, userId: string, limit: int, sinceId: string, untilId: string): JsonNode =
+  let body = %*{
+    "i": token,
+    "userId": userId,
+    "limit": limit,
+    "sinceId": sinceId,
+    "untilId": untilId
+  }
+  return requestMisskey("https://misskey.m544.net/api/users/notes", httpMethod = HttpPost, body = $body)
+
+proc getNotes*(token: string, userId: string, limit: int, untilId: string): JsonNode =
+  let body = %*{
+    "i": token,
+    "userId": userId,
+    "limit": limit,
+    "untilId": untilId
+  }
+  return requestMisskey("https://misskey.m544.net/api/users/notes", httpMethod = HttpPost, body = $body)
 
 proc getNotes*(token: string, userId: string, limit: int): JsonNode =
   let body = %*{
@@ -13,7 +33,4 @@ proc getNotes*(token: string, userId: string, limit: int): JsonNode =
     "userId": userId,
     "limit": limit
   }
-  echo body
-  let response = client.request("https://misskey.m544.net/api/users/notes", httpMethod = HttpPost, body = $body)
-  echo response.status
-  return response.body.parseJson()
+  return requestMisskey("https://misskey.m544.net/api/users/notes", httpMethod = HttpPost, body = $body)
